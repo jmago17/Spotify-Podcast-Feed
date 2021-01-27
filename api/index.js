f1168b26ea281154476365e742dc5f9878722c0a
Benny Samir Hierl <bennysamir@posteo.de>
2021-01-27 17:52:28 +0100

    check if id is valid
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- api/index.js
+++ api/index.js
@@ -2,8 +2,14 @@ import Podcast from 'podcast';
 const { send } = require('micro')
 const spotify = require('./_utils/spotify')
 
+const regex = new RegExp('foo*')
+
 module.exports = async (req, res) => {
   try {
+    if (regex.test(req.query.id) === false) {
+      throw new Error('Invalid show id')
+    }
+    
     const show = await spotify.getShow(req.query.id)
 
     const feed = new Podcast({
